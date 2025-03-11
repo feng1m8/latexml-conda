56e7fc1c61197914ab1b74d5fb6bc5b2ba86e645
Minato Feng <feng1m8@outlook.com>
2025-03-11 20:29:05 +0800

    Use LIBRARY_PREFIX

--- perl-image-magick/bld.bat
+++ perl-image-magick/bld.bat
@@ -1,5 +1,4 @@
-perl Makefile.PL INC="-I%BUILD_PREFIX%\Library\ucrt64\include\ImageMagick-7" MYEXTLIB="%BUILD_PREFIX%\Library\ucrt64\bin\libMagickCore-7.Q16HDRI-10.dll" CCCDLFLAGS="-DMAGICKCORE_HDRI_ENABLE"
+perl Makefile.PL INC="-I%LIBRARY_PREFIX%\ucrt64\include\ImageMagick-7" MYEXTLIB="%LIBRARY_PREFIX%\ucrt64\bin\libMagickCore-7.Q16HDRI-10.dll" CCCDLFLAGS="-DMAGICKCORE_HDRI_ENABLE"
 
 make
-make test
 make install
