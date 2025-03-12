e8cf7b6b653f9d6852c8ff1cdf5700de03d3c933
Minato Feng <feng1m8@outlook.com>
2025-03-12 09:37:11 +0800

    Add symlink exe

--- perl-latexml/bld.bat
+++ perl-latexml/bld.bat
@@ -2,3 +2,15 @@ perl Makefile.PL
 
 make
 make install
+
+mkdir %PREFIX%\Scripts
+
+cl %RECIPE_DIR%\latexml.cxx %BUILD_PREFIX%\Library\symlink-exe.c -DLATEXML=\"latexml\" -DRELATIVE_PATH -std:c++17 -EHsc -O1 -MD -Fe%PREFIX%\Scripts\latexml.exe
+
+cl %RECIPE_DIR%\latexml.cxx %BUILD_PREFIX%\Library\symlink-exe.c -DLATEXML=\"latexmlc\" -DRELATIVE_PATH -std:c++17 -EHsc -O1 -MD -Fe%PREFIX%\Scripts\latexmlc.exe
+
+cl %RECIPE_DIR%\latexml.cxx %BUILD_PREFIX%\Library\symlink-exe.c -DLATEXML=\"latexmlfind\" -DRELATIVE_PATH -std:c++17 -EHsc -O1 -MD -Fe%PREFIX%\Scripts\latexmlfind.exe
+
+cl %RECIPE_DIR%\latexml.cxx %BUILD_PREFIX%\Library\symlink-exe.c -DLATEXML=\"latexmlmath\" -DRELATIVE_PATH -std:c++17 -EHsc -O1 -MD -Fe%PREFIX%\Scripts\latexmlmath.exe
+
+cl %RECIPE_DIR%\latexml.cxx %BUILD_PREFIX%\Library\symlink-exe.c -DLATEXML=\"latexmlpost\" -DRELATIVE_PATH -std:c++17 -EHsc -O1 -MD -Fe%PREFIX%\Scripts\latexmlpost.exe
