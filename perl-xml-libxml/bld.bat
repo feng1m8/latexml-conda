41c253bb0d152510cf0a7430cb3f727e197fce40
Yuan Feng <feng1m8@outlook.com>
2025-09-21 21:31:30 +0800

    Update perl-xml-libxml

--- perl-xml-libxml/bld.bat
+++ perl-xml-libxml/bld.bat
@@ -1,4 +1,4 @@
-perl Makefile.PL INC="-I%LIBRARY_PREFIX%\mingw-w64\include -I%LIBRARY_PREFIX%\mingw-w64\include\libxml2" MYEXTLIB="%LIBRARY_PREFIX%\mingw-w64\bin\libxml2-2.dll" CCCDLFLAGS="-Wno-error=incompatible-pointer-types"
+perl Makefile.PL INC="-I%LIBRARY_PREFIX%\mingw-w64\include -I%LIBRARY_PREFIX%\mingw-w64\include\libxml2" MYEXTLIB="%LIBRARY_PREFIX%\mingw-w64\lib\libxml2.a %LIBRARY_PREFIX%\mingw-w64\lib\libiconv.a %LIBRARY_PREFIX%\mingw-w64\lib\libz.a %LIBRARY_PREFIX%\mingw-w64\lib\liblzma.a" CCCDLFLAGS="-DLIBXML_STATIC -Wno-error=incompatible-pointer-types"
 
 make
 make test
